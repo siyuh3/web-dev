d5437d038220457ab488615cb76e6b3f2efb1a69
siyu3 <meikoshans@gmail.com>
2021-10-07 20:49:42 -0700

    a4 bootstrap fa21

--- public/a5/practice/js/index.js
+++ public/a5/practice/js/index.js
@@ -1,1 +1,16 @@
-alert('Hello World!');
+//alert('Hello World!');
+// eslint-disable-next-line no-unused-vars
+/* global _ */
+console.log('Hello World!');
+
+let global1 = 10;
+var functionScoped = 2;
+let blockScoped = 5;
+const constant1 = global1 + functionScoped - blockScoped;
+
+
+console.log("Variables and constants");
+console.log(global1);
+console.log(functionScoped);
+console.log(blockScoped);
+console.log(constant1);
