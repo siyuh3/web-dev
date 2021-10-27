c182fcca6d1db1ddc5397b64ea819b71cda1a42b
siyu3 <meikoshans@gmail.com>
2021-10-26 18:09:50 -0700

    up

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './vendors/bootstrap/css/bootstrap.min.css';
 import './vendors/bootstrap/bootstrap.min.css';
 //import './vendors/fontawesome/css/all.min.css';
 //import './vendors/fontawesome/css/fontawesome.css';
-import './vendors/fontawesome/css/fontawesome.min.css';
+//import './vendors/fontawesome/css/fontawesome.min.css';
 import HelloWorld from "./components/a6/HelloWorld";
 import Practice from "./components/a6/Practice";
 import Build from "./components/a6/Build";
